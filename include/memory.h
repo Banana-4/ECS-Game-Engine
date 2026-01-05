b5fac8101684e349b010a51e656ebab7e06cad88
Banana-4 <stig34768@gmail.com>
2026-01-05 20:31:54 +0100

    add packed array and its interface

--- include/memory.h
+++ include/memory.h
@@ -1,5 +1,21 @@
-#include "../include/component.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define POS_STRIDE 2
 
-void* get_components(ComponentsID id);
-Entity* get_entities();
+//stores data that contains two ints
+typedef struct {
+  int* data;
+  int capacity;
+  int size;
+} PackedArray;
+
+bool pa_init(PackedArray* pa, int capacity);
+bool pa_push(PackedArray* pa, int x, int y);
+bool pa_remove(PackedArray* pa, int n);
+void pa_free(PackedArray* pa);
+void pa_print(PackedArray* pa, char* msg);
+
+
+
 
