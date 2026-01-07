be7e1d27005b1641b0c8182339aad7feee9172f9
Banana-4 <stig34768@gmail.com>
2026-01-07 22:26:00 +0100

    implement packed array with ids

--- src/memory.c
+++ src/memory.c
@@ -1,80 +1,96 @@
 #include "../include/memory.h"
 
-bool pa_init(PackedArray* pa, int capacity) {
-  if (!pa || capacity <= 0) {
+bool pp_init(PackedPositions* pp, int capacity) {
+  if (!pp || capacity <= 0) {
     return false;
   }
-  pa->data = (int*)malloc(sizeof(int) * capacity * POS_STRIDE);
-  
-  if(!pa->data) {
+  pp->x = (int*)malloc(sizeof(int) * capacity);
+  if(!pp->x) { return false; }
+
+  pp->y =  (int*)malloc(sizeof(int) * capacity);
+  if(!pp->y) {
+    free(pp->x);
     return false;
   }
-  
+
+  pp->cmp_id = (int*) malloc(sizeof(int) * capacity);
+  if(!pp->cmp_id) {
+    free(pp->x);
+    free(pp->y);
+    return false;
+  }
+
+  for (int i = 0; i < MAX_ENTITIES; i++) 
+    pp->id_map[i] = -1;
+
   pa->capacity = capacity;
   pa->size = 0;
 
   return true;
 }
 
-bool pa_push(PackedArray* pa, int x, int y) {
-  if(!pa->data) {
+bool pp_push(PackedPositions* pp, int id, int x, int y) {
+  if(!pp->data) {
     return false;
   }
 
-  if(pa->size == pa->capacity) {
+  if(pp->size == pa->capacity) {
     return false;
   }
+  
+  pp->x[pp->size] = x;
+  pp->y[pp->size] = y;
+  
+  pp->cmp_id[pp->size] = id;
+  pp->id_map[id] = pp->size;
 
-  pa->data[pa->size * POS_STRIDE] = x;
-  pa->data[pa->size * POS_STRIDE + 1] = y;
-
-  pa->size++;
+  pp->size++;
 
   return true;
 
 }
 
-bool pa_remove(PackedArray* pa, int n) {
-  if(!pa || !pa->data) {
+bool pp_remove(PackedPositions* pp, int i) {
+  if(!pp || !pp->x || !pp->y) {
     return false;
   }
   
-  if (n >= pa->size) {
+  if (i >= pp->size || i < 0) {
     return false;
   }
-
-  int index = n * POS_STRIDE - POS_STRIDE;
-
-  int tmp_x = pa->data[pa->size * POS_STRIDE - 1];
-  int tmp_y = pa->data[pa->size * POS_STRIDE - 2];
-
-  pa->data[pa->size * POS_STRIDE - 1] = pa->data[index];
-  pa->data[pa->size * POS_STRIDE - 2] = pa->data[index + 1];
   
-  pa->data[index] = tmp_x;
-  pa->data[index + 1] = tmp_y;
+  pp->size--;
+  pp->x[i] = pp->x[pp->size];
+  pp->y[i] = pp->y[pp->size];
   
-  pa->size--;
+  int id = pp->cmp_id[pp->size];
+  pp->id_map[id] = i;
+  pp->cmp_id[i] = id;
+
+  pp->id_map[i] = -1;
 
   return true;
 }
 
-void pa_free(PackedArray* pa) {
-  if (!pa) return;
-  free(pa->data);
-  pa->size = 0;
-  pa->capacity = 0;
-  pa->data = NULL;
+void pp_free(PackedPositions* pp) {
+  if (!pp) return;
+  free(pp->x);
+  free(pp->y);
+  free(pp->cmp_id);
+
+  pp->size = 0;
+  pp->capacity = 0;
+  pp->x = NULL;
+  pp->y = NULL;
+  pp->cmp_id = NULL;
 }
 
-void pa_print(PackedArray* pa, char* msg) {
-  if(!pa) {
+void pp_print(PackedPositions* pp) {
+  if(!pp) {
     printf("No position components");
     return;
   }
-  int j = POS_STRIDE;
   for(int i = 0; i < pa->size; i++) {
-    printf(msg, i, pa->data[j - POS_STRIDE], pa->data[j - 1]);
-    j += POS_STRIDE;
+    printf("Index: %d X: %d Y: %d\n", i, pp->x[i], pp->y[i]);
   }
 }
