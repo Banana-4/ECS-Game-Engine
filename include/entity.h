32b8befd47b3c369631f7819cbf70f480f1435f8
Banana-4 <stig34768@gmail.com>
2026-01-13 16:09:26 +0100

    add the interface for the entity module

--- include/entity.h
+++ include/entity.h
@@ -1,4 +1,8 @@
-typedef struct {
-  int id;
-  int components; // set bits represent components that the entity has
-} Entity;
+#include "memory.h"
+
+bool init_entities(int capacity);
+bool create_entity(int cmp_mask);
+bool remove_entity(int id);
+
+PackedEntities* get_entites();
+
