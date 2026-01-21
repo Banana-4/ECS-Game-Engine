ec2334b8bbd57a9d808eae1431a840dbb278041c
Banana-4 <stig34768@gmail.com>
2026-01-21 22:40:14 +0100

    add  iterator for entities

--- include/entity.h
+++ include/entity.h
@@ -1,8 +1,21 @@
 #include "memory.h"
 
+
+typedef struct {
+    int* id;
+    int* cmp_mask;
+    int pos;
+} EnIter;
+
 bool init_entities(int capacity);
 bool create_entity(int cmp_mask);
 bool remove_entity(int id);
+void free_entities();
 
-PackedEntities* get_entites();
+EnIter* create_enIter();
+void next_enIter(EnIter* iter);
+int id_enIter(EnIter* iter);
+int mask_enIter(EnIter* iter);
+void free_enIter(EnIter* iter);
+
 
