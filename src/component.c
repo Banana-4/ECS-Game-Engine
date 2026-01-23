c3b238211bfeaeb38de1187dc161297666c4dc40
Banana-4 <stig34768@gmail.com>
2026-01-23 21:54:10 +0100

    add PackedASCII, fix bad name ps, & in get funct

--- src/component.c
+++ src/component.c
@@ -5,7 +5,7 @@ PackedPositions* pp;
 PackedHealths* ph;
 PackedVelocities* pv;
 PackedAttacks *pa;
-PackedSprites* ps;
+PackedASCII* ps;
 
 
 
@@ -23,7 +23,7 @@ bool init_stores(int capacity) {
     ph_free(ph);
     return false;
   }
-  if (!ps_init(ps, capacity)) {
+  if (!pas_init(ps, capacity)) {
     pp_free(pp);
     ph_free(ph);
     pv_free(pv);
@@ -33,7 +33,7 @@ bool init_stores(int capacity) {
     pp_free(pp);
     ph_free(ph);
     pv_free(pv);
-    ps_free(ps);
+    pas_free(ps);
     return false;
   }
   return true;
@@ -43,32 +43,26 @@ void free_stores() {
   pp_free(pp);
   ph_free(ph);
   pv_free(pv);
-  ps_free(ps);
+  pas_free(ps);
   pa_free(pa);
 }
 
-PosIter* get_PosIter() {
-  PosIter *iter = (PosIter *)malloc(sizeof(PosIter));
-  iter->x = pp->x;
-  iter->y = pp->y;
-  return iter;
-}
 
 void get_postion(int id, ppData* out) {
-    out->x = pp->x[pp->id_map[id]];
-    out->y = pp->y[pp->id_map[id]];
+    out->x = &pp->x[pp->id_map[id]];
+    out->y = &pp->y[pp->id_map[id]];
 }
 void get_velocity(int id, pvData* out) {
-    out->x = pv->x[pv->id_map[id]];
-    out->y = pv->y[pv->id_map[id]];
+    out->x = &pv->x[pv->id_map[id]];
+    out->y = &pv->y[pv->id_map[id]];
 }
 
 void get_health(int id, phData* out) {
-  out->hp = ph->hp[ph->id_map[id]];
+  out->hp = &ph->hp[ph->id_map[id]];
 }
 
 void get_attack(int id, paData* out) {
-  out->dmg = pa->dmg[pa->id_map[id]];
+  out->dmg = &pa->dmg[pa->id_map[id]];
 }
 
 void get_ascii(int id, pasData* out) {
