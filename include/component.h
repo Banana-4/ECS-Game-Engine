8cb74c7d0d101e6e9e11428e575d53d282f52323
Banana-4 <stig34768@gmail.com>
2026-01-27 11:08:17 +0100

    add update and read only interfaces

--- include/component.h
+++ include/component.h
@@ -1,29 +1,38 @@
 #include "memory.h"
 
 typedef struct ppData {
-    int *x;
-    int *y;
+    int x;
+    int y;
 }ppData;
+
 typedef struct pvData {
-    int *x;
-    int *y;
+    int x;
+    int y;
 }pvData;
 
 typedef struct phData {
-    double* hp;
+    double hp;
 }phData;
 
 typedef struct paData {
-    double* dmg;
+    double dmg;
 }paData;
 
 typedef struct pasData {
-    char* ascii;
+    char ascii;
 }pasData;
 
 bool init_stores(int capacity);
 void free_stores();
 
+//add new components, create or update
+bool position_insert(int id, int x, int y);
+bool velocity_insert(int id, int x, int y);
+bool health_insert(int id, double hp);
+bool ascii_insert(int id, char ch);
+bool attack_insert(int id, double dmg);
+
+//read components no change to data
 void get_postion(int id, ppData* out);
 void get_velocity(int id, pvData* out);
 void get_health(int id, phData* out);
