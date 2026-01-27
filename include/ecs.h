a91b699e8c7d4a28f70eb6da584304f6fb72d147
Banana-4 <stig34768@gmail.com>
2026-01-27 10:52:58 +0100

    add interface for creating and removing entities

--- include/ecs.h
+++ include/ecs.h
@@ -5,9 +5,3 @@ void ecs_stop();
 
 bool ecs_create(int cmpMask);
 bool ecs_remove(int id);
-
-EnIter* ecs_keys();
-void ecs_next(EnIter* iter);
-int ecs_key(EnIter* iter);
-int ecs_mask(EnIter* iter)
-void ecs_freeIter(EnIter* iter);
