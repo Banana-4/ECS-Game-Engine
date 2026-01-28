72c19a38d2ddf3c878438dc5abccbeba7f90ed67
Banana-4 <stig34768@gmail.com>
2026-01-28 20:44:59 +0100

    reworked the interface and created a iterator

--- include/entity.h
+++ include/entity.h
@@ -2,20 +2,27 @@
 
 
 typedef struct {
-    int* id;
-    int* cmp_mask;
-    int pos;
+    int id;
+    unsigned int cmp_mask;
+    int left;
 } EnIter;
 
 bool init_entities(int capacity);
-bool create_entity(int cmp_mask);
+int create_entity(int cmp_mask);
+bool update_mask_entity(int id, int cmp_mask);
+
 bool remove_entity(int id);
 void free_entities();
 
+
+
+//read only
 EnIter* create_enIter();
-void next_enIter(EnIter* iter);
+bool next_enIter(EnIter* iter);
 int id_enIter(EnIter* iter);
 int mask_enIter(EnIter* iter);
 void free_enIter(EnIter* iter);
 
+//debug
 
+void print_entites_store();
