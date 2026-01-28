be659ed01b79a1e9ab27bda39828be2494b72937
Banana-4 <stig34768@gmail.com>
2026-01-28 20:45:34 +0100

    reworked the interface and created a iterator

--- src/entity.c
+++ src/entity.c
@@ -10,34 +10,44 @@ static int get_id() {
   if (freeIDsize >= 0) {
     return freeIds[freeIDsize--];
   }
-  if(nextID == MAX_ENTITIES) return -1;
-
+  if(nextID == MAX_ENTITIES - 1) return -1;
   return nextID++;
 }
 
 bool init_entities(int capacity) {
   pe = (PackedEntities*)malloc(sizeof(PackedEntities));
+  if (!pe)
+      return false;
   return pe_init(pe, capacity);
+
 }
 
-bool create_entity(int cmp_mask) {
+int create_entity(int cmp_mask) {
   int id = get_id();
-  if (id < 0) return false;
-  return pe_push(pe, id, cmp_mask);
+  if (id < 0) return -1;
+  if(!pe_insert(pe, id, cmp_mask)) {
+      freeIds[++freeIDsize] = id;
+      return -1;
+  }
+  return id;
+}
+
+bool update_mask_entity(int id, int cmp_mask) {
+    if(pe_has(pe,id)) {
+        pe_insert(pe, id, cmp_mask);
+        return true;
+    }
+    return false;
 }
 
 bool remove_entity(int id) {
-  if( pe_remove(pe, id)) {
+  if(pe_remove(pe, id)) {
     freeIds[++freeIDsize] = id;
     return true;
   }
   return false;
 }
 
-
-PackedEntities* get_entites() {
-    return pe;
-}
 
 void kill_entities() {
   pe_free(pe);
@@ -47,26 +57,29 @@ void kill_entities() {
 
 EnIter* create_enIter() {
   EnIter* iter = (EnIter*)malloc(sizeof(EnIter));
-  if (!iter) return NULL;
-  iter->id = pe->id;
-  iter->cmp_mask = pe->cmp_mask;
-  iter->pos = 0;
+
+  if (!iter)
+      return NULL;
+
+  iter->left = pe->size;
+  iter->id = pe->id[pe->size - iter->left];
+  iter->cmp_mask = pe->cmp_mask[pe->size - iter->left];
   return iter;
+
 }
+
 bool next_enIter(EnIter* iter) {
-  if(iter->pos < pe->size) {
-    iter->id++;
-    iter->cmp_mask++;
-    iter->pos++;
+  if(--iter->left >= 1) {
+    iter->id = pe->id[pe->size - iter->left];
+    iter->cmp_mask = pe->cmp_mask[pe->size - iter->left];
     return true;
   }
-  
    return false;
 }
 
 int id_enIter(EnIter* iter) {
   if(iter) {
-    return *(iter->id);
+    return iter->id;
   }
   return -1;
 }
@@ -74,7 +87,7 @@ int id_enIter(EnIter* iter) {
 
 int mask_enIter(EnIter* iter) {
    if(iter) {
-    return *(iter->cmp_mask);
+    return iter->cmp_mask;
   }
   return -1;
 }
@@ -82,4 +95,8 @@ int mask_enIter(EnIter* iter) {
 void free_enIter(EnIter* iter) {
   free(iter);
   iter = NULL;
+}
+
+void print_entites_store() {
+    pe_print(pe);
 }
