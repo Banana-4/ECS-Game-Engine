a484bbf6e81ecd2272e12c577287396881376191
Banana-4 <stig34768@gmail.com>
2026-01-30 12:59:55 +0100

    Rework component interface to clarify ownership and responsibilities

--- include/component.h
+++ include/component.h
@@ -1,40 +1,86 @@
-#include "memory.h"
+#include <stdbool.h>
 
-typedef struct ppData {
-    int x;
-    int y;
-}ppData;
+//used for point coordiantes, position velocity
+struct vec2Iter {
+    int *x;
+    int *y;
+    int *cmp_id;
+    int left;
+};
 
-typedef struct pvData {
-    int x;
-    int y;
-}pvData;
+struct scalarDIter {
+    double *val;
+    int *cmp_id;
+    int left;
+};
 
-typedef struct phData {
-    double hp;
-}phData;
+struct scalarChIter {
+    char *value;
+    int *cmp_id;
+    int left;
+};
 
-typedef struct paData {
-    double dmg;
-}paData;
-
-typedef struct pasData {
-    char ascii;
-}pasData;
+typedef struct vec2Iter posIter;
+typedef struct vec2Iter velIter;
+typedef struct scalarDIter hpIter;
+typedef struct scalarDIter attIter;
+typedef struct scalarChIter pasIter;
 
 bool init_stores(int capacity);
 void free_stores();
 
-//add new components, create or update
+//insert data interface
+
 bool position_insert(int id, int x, int y);
 bool velocity_insert(int id, int x, int y);
 bool health_insert(int id, double hp);
 bool ascii_insert(int id, char ch);
 bool attack_insert(int id, double dmg);
 
-//read components no change to data
-void get_postion(int id, ppData* out);
-void get_velocity(int id, pvData* out);
-void get_health(int id, phData* out);
-void get_attack(int id, paData* out);
-void get_ascii(int id, pasData* out);
+//remove interface
+
+bool position_remove(int id);
+bool velocity_remove(int id);
+bool health_remove(int id);
+bool ascii_remove(int id);
+bool attack_remove(int id);
+
+//get
+
+//iteration interface
+
+//position
+void posIter_init(posIter *iter);
+bool posIter_next(posIter *iter);
+int posIter_getX(posIter *iter);
+int posIter_getY(posIter *iter);
+int posIter_getID(posIter *iter);
+
+//velocity
+void velIter_init(velIter *iter);
+bool velIter_next(velIter *iter);
+int velIter_getX(velIter *iter);
+int velIter_getY(velIter *iter);
+int velIter_getID(velIter *iter);
+
+//health
+
+void hpIter_init(hpIter *iter);
+bool hpIter_next(hpIter *iter);
+double hpIter_getHP(hpIter *iter);
+int hpIter_getID(hpIter *iter);
+
+// Attack
+
+void attIter_init(attIter *iter);
+bool attIter_next(attIter *iter);
+double attIter_getDmg(attIter *iter);
+int attIter_getID(attIter *iter);
+
+
+//ASCII ART
+
+void pasIter_init(pasIter *iter);
+bool pasIter_next(pasIter *pas);
+char pasIter_getCh(pasIter *pas);
+int pasIter_getID(pasIter *pas);
