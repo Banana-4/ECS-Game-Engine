69f43f6bceb3133d314a5c7cd3475386397010e9
Banana-4 <stig34768@gmail.com>
2026-02-13 10:47:14 +0100

    Enforce memory module responsibility
    Move store implementations to memory.c. Change the stores size and capacity to size_t

--- include/memory.h
+++ include/memory.h
@@ -2,33 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_ENTITIES 10000
+// Stores
+typedef struct PackedEntities PackedEntities;
+typedef struct PackedVec2Int PackedPositions;
+typedef struct PackedVec2Int PackedVelocities;
+typedef struct PackedDouble PackedHealths;
+typedef struct PackedChar PackedASCII;
+typedef struct PackedDouble PackedAttacks;
 
-typedef struct {
-  int* id;
-  unsigned* cmp_mask;
-  int capacity;
-  int size;
-  
-  int id_map[MAX_ENTITIES];
-} PackedEntities;
-
-bool pe_init(PackedEntities* pe, int capacity);
+// Entity store interface
+bool pe_init(PackedEntities* pe, size_t capacity);
 bool pe_insert(PackedEntities* pe, int id, unsigned mask);
+bool pe_has(PackedEntities* pe, int id);
 bool pe_remove(PackedEntities* pe, int id);
 void pe_free(PackedEntities* pe);
 void pe_print(PackedEntities* pe);
 
-typedef struct {
-  int* x;
-  int* y;
-  int capacity;
-  int size;
-  
-  int* cmp_id;
-  int id_map[MAX_ENTITIES];
-} PackedPositions;
-
-bool pp_init(PackedPositions* pp, int capacity);
+//Postion store interface
+bool pp_init(PackedPositions* pp, size_t capacity);
 bool pp_insert(PackedPositions* pp, int id, int x, int y);
 
 bool pp_has(PackedPositions* pp, int id);
@@ -38,35 +29,16 @@ void pp_free(PackedPositions* pp);
 void pp_print(PackedPositions* pp);
 
 
-
-typedef struct {
-  double* hp;
-  int capacity;
-  int size;
-  
-  int* cmp_id;
-  int id_map[MAX_ENTITIES];
-} PackedHealths;
-
-bool ph_init(PackedHealths* ph, int capacity);
+// Health store interface
+bool ph_init(PackedHealths* ph, size_t capacity);
 bool ph_insert(PackedHealths* ph, int id, double hp);
 bool ph_has(PackedHealths* ph, int id);
 bool ph_remove(PackedHealths* ph, int id);
 void ph_free(PackedHealths* ph);
 void ph_print(PackedHealths* ph);
 
-typedef struct {
-  int *x;
-  int *y;
-
-  int capacity;
-  int size;
-
-  int* cmp_id;
-  int id_map[MAX_ENTITIES];
-} PackedVelocities;
-
-bool pv_init(PackedVelocities* pv, int capacity);
+//Velocity store interface
+bool pv_init(PackedVelocities* pv, size_t capacity);
 bool pv_insert(PackedVelocities* pv, int id, int x, int y);
 bool pv_has(PackedVelocities* pv, int id);
 bool pv_remove(PackedVelocities* pv, int id);
@@ -74,33 +46,16 @@ void pv_free(PackedVelocities* pv);
 void pv_print(PackedVelocities* pv);
 
 
-typedef struct {
-  char* ascii;
-  int capacity;
-  int size;
-
-  int* cmp_id;
-  int id_map[MAX_ENTITIES];
-} PackedASCII;
-
-bool pas_init(PackedASCII* pas, int capacity);
+// ASCII store interface
+bool pas_init(PackedASCII* pas, size_t capacity);
 bool pas_insert(PackedASCII* pas, int id, char ch);
 bool pas_has(PackedASCII* pas, int id);
 bool pas_remove(PackedASCII* pas, int id);
 void pas_free(PackedASCII* pas);
 void pas_print(PackedASCII* pas);
 
-
-typedef struct {
-  double* dmg;
-  int capacity;
-  int size;
-
-  int *cmp_id;
-  int id_map[MAX_ENTITIES];
-} PackedAttacks;
-
-bool pa_init(PackedAttacks *pa, int capacity);
+//Attack store interface
+bool pa_init(PackedAttacks *pa, size_t capacity);
 bool pa_insert(PackedAttacks* pa, int id, double dmg);
 bool pa_has(PackedAttacks* pa, int id);
 bool pa_remove(PackedAttacks* pa, int id);
