04b6d1bd5633fd8941dd06072b0a94073cbd421b
Banana-4 <stig34768@gmail.com>
2026-02-26 19:24:18 +0000

    restrict entity module to ID management
    The entity module is now responsible for creating and recycling IDs.

--- include/entity.h
+++ include/entity.h
@@ -1,35 +1,7 @@
 #ifndef MEMORY_H
 #define MEMORY_H
-#include "memory.h"
+#include "../include/memory.h"
 #endif
 
-typedef struct {
-    int id;
-    unsigned int cmp_mask;
-    int left;
-} enIter;
-
-bool init_entities(int capacity);
-int create_entity(int cmp_mask);
-bool update_mask_entity(int id, int cmp_mask);
-
-bool remove_entity(int id);
-void free_entities();
-
-
-//iterators
-
-static inline int iter_left(enIter iter) { return iter.left; }
-static inline bool iter_valid(enIter iter) { return iter.left != 0; }
-
-
-bool enIter_init(enIter *iter);
-bool enIter_next(enIter *iter);
-bool enIter_getID(enIter *iter, int *out);
-bool enIter_getMask(enIter *iter, unsigned *out);
-void enIter_free(enIter *iter);
-
-
-//output
-int en_dump();
-void print_entites_store();
+int get_id();
+void recycle_id(int id);
